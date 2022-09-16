3839bf544228556ff235579638c7f6e9102550ca
Dell <Dell@DESKTOP-0L7J8AE>
2022-09-15 20:41:58 -0700

    [Nikita]:4th changes in code

---
+++ src/sample/demo3.java
@@ -0,0 +1,18 @@
+package sample;
+
+public class demo3
+{
+	public static void main(String[] args) 
+	{
+
+		System.out.println("hieeeeeeeeeeeeee");
+		System.out.println("hieeeeeeeeeeeeee");
+		System.out.println("hieeeeeeeeeeeeee");
+		System.out.println("hieeeeeeeeeeeeee");
+		System.out.println("hieeeeeeeeeeeeee");
+		System.out.println("hieeeeeeeeeeeeee");
+		System.out.println("hieeeeeeeeeeeeee");
+		System.out.println("hieeeeeeeeeeeeee");
+	}
+
+}
